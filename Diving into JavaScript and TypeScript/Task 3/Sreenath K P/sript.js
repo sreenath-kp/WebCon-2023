8c4c8f2e28f55f8a6cedde91fbe23a54b733f5a4
Sreenath <108588566+sreenath-kp@users.noreply.github.com>
2023-04-30 13:25:23 +0530

    not responsive

--- Diving into JavaScript and TypeScript/Task 3/Sreenath K P/sript.js
+++ Diving into JavaScript and TypeScript/Task 3/Sreenath K P/sript.js
@@ -1,21 +1,20 @@
 var Top = 0;
-    header = document.querySelector('header');
-window.addEventListener('scroll', function () {
-    var scrollTop = window.scrollY || document.documentElement.scrollTop;
-    if (scrollTop > Top) {
-        header.style.top = "-8vh";
-    }else {
-        header.style.top = "0";
-    }
-})    
+header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+  var scrollTop = window.scrollY || document.documentElement.scrollTop;
+  if (scrollTop > Top) {
+    header.style.top = "-8vh";
+  } else {
+    header.style.top = "0";
+  }
+});
 
-var searchIcon = document.querySelector('.search-icon');
-var search = document.querySelector('.search');
-var cross = document.querySelector('.cross');
+var searchIcon = document.querySelector(".search-icon");
+var search = document.querySelector(".search");
+var cross = document.querySelector(".cross");
 
-searchIcon.addEventListener('click',function () {
-    search.classList.toggle('active');
-    searchIcon.classList.toggle('active');
-    cross.classList.toggle('active');
-
-})
+searchIcon.addEventListener("click", function () {
+  search.classList.toggle("active");
+  searchIcon.classList.toggle("active");
+  cross.classList.toggle("active");
+});
