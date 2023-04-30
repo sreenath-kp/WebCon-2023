647f00648d64b4f24775b517baecd1fa288a4b67
Sreenath <108588566+sreenath-kp@users.noreply.github.com>
2023-04-30 13:24:27 +0530

    static

--- Diving into JavaScript and TypeScript/Task 3/Sreenath K P/sript.js
+++ Diving into JavaScript and TypeScript/Task 3/Sreenath K P/sript.js
@@ -1,3 +1,14 @@
+var Top = 0;
+    header = document.querySelector('header');
+window.addEventListener('scroll', function () {
+    var scrollTop = window.scrollY || document.documentElement.scrollTop;
+    if (scrollTop > Top) {
+        header.style.top = "-8vh";
+    }else {
+        header.style.top = "0";
+    }
+})    
+
 var searchIcon = document.querySelector('.search-icon');
 var search = document.querySelector('.search');
 var cross = document.querySelector('.cross');
